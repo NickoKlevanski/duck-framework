9dcdec813a56c0257a7f36545e27d1f16c990bf0
NickoKlevanski <nikolai.klevanski@gmail.com>
2021-06-23 00:19:49 +0300

    Event handler for modal

--- index.js
+++ index.js
@@ -7,9 +7,30 @@ if (module.hot) {
 
 window.dataStore = {
   currentFilm: '',
+  films: [],
 };
 
 window.renderApp = renderApp;
+window.handleClick = handleClick;
+
+function handleClick(id) {
+  const list = document.querySelectorAll('li');
+  for (let item of list) {
+    item.addEventListener('click', test);
+  }
+}
+
+function test(e) {
+  const filmId = e.target.id;
+  const filmObj = window.dataStore.films.find(elem => elem.id === filmId);
+  // const body = document.querySelector('body');
+  // const block = `
+  //   <div>
+  //     <div>test</div>
+  //   </div>
+  // `
+  // body.innerHTML += block;
+}
 
 renderApp();
 
@@ -21,7 +42,7 @@ function App() {
   return `<div>
     ${SearchByFilm()}
     <br/>
-    ${FilmListResult()}
+    ${FilmListResult(handleClick)}
   </div>`;
 }
 
@@ -29,10 +50,10 @@ function SearchByFilm() {
   return `<input 
     type="text"
     value="${window.dataStore.currentFilm}"
-    onchange="window.dataStore.currentFilm = this.value; renderApp();"/>`;
+    onchange="window.dataStore.currentFilm = this.value; renderApp(); handleClick();"/>`;
 }
 
-function FilmListResult() {
+function FilmListResult(handleClickFn) {
   const { currentFilm } = window.dataStore;
   const regex = new RegExp(currentFilm, 'i');
   const films = data.results.filter(film => {
@@ -40,13 +61,19 @@ function FilmListResult() {
       return film.original_title.match(regex);
     }
   });
+  window.dataStore.films = films;
   let content = '';
   if (films.length) {
     content += '<h3>Lists of films</h3>';
     content += `<ul class="list">${films
       .map(
-        ({ original_title, release_date }) =>
-          `<li class="item">${original_title}<br/>${release_date}</li>`,
+        ({ original_title, release_date, id }) =>
+          `<li class="item" id="${id}"
+          >
+            ${original_title}
+            <br/>
+            ${release_date}
+          </li>`,
       )
       .join('')}</ul>`;
   }
